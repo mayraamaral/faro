4b7f251322e9a29d98ba78db34f5b9cad36263f2
Mayra Amaral <mayracunha18@gmail.com>
2026-06-25 16:57:52 -0300

    feat: adiciona tela de status

---
+++ app/(app)/adoption-status/[adoption_id].tsx
@@ -0,0 +1,5 @@
+import { AdoptionStatusScreen } from "@/features/adoptions/components/adoption-status-screen";
+
+export default function AdoptionStatusRoute() {
+  return <AdoptionStatusScreen />;
+}
